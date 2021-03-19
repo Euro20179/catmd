fa85b718c3b5568da79de23469dd39e4e8191da7
Euro <no>
2021-03-19 13:52:28 -0700

    error + defualt to README.md

--- catmd.cpp
+++ catmd.cpp
@@ -59,10 +59,15 @@ std::string regex_replace(const std::string& s,
 
 int main(int argc, const char *argv[])
 {
-    std::ifstream file;
-
     const char* fileName = argv[1];
-    file.open(fileName, std::ios::in);
+    if(!fileName){
+	fileName="README.md";
+    }
+    std::ifstream file (fileName);
+    if(!file.is_open()){
+	std::cout << "\033[31mError: invalid file name\033[0m" << std::endl;
+	return 1;
+    }
 
     std::string line;
     std::string fullData;
